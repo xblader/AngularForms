dd49cc19a0a4ed458593a15dd831fb6d26990c51
CY47 <CY47@petrobras.com.br>
2017-06-08 10:35:31 -0300

    modulo 73

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
+import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: 'templateForm', component: TemplateFormComponent },
+  { path: 'dataForm', component: DataFormComponent }
 ];
 
 @NgModule({
