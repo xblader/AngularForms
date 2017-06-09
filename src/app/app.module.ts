687ee536d1898681326fb2a356dffb17b3082426
xblader <xblader@gmail.com>
2017-06-09 20:27:48 -0300

    modulo 76

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TemplateFormModule } from './template-form/template-form.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -5,19 +6,18 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TemplateFormComponent,
+    AppComponent,    
     DataFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    TemplateFormModule,
     AppRoutingModule
   ],
   providers: [],
