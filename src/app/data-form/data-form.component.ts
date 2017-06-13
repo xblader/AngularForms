6a56c26b1d1928e2e49e9bb387fbed52715fe2de
CY47 <CY47@petrobras.com.br>
2017-06-13 08:30:41 -0300

    validation reactive forms

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -13,14 +13,64 @@ export class DataFormComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder, private http: Http) { }
 
+  hero : any = {};
+ 
+  submitted = false;  
+
   ngOnInit() {
-    this.formulario = this.formBuilder.group({
-      nome: [null,  Validators.required],
-      email: [null]
-    });
+    this.buildForm();    
   }
 
+  buildForm(){
+    this.formulario = this.formBuilder.group({
+      nome: [null,  [Validators.required, Validators.minLength(4), Validators.maxLength(24)]],
+      email: [null]
+    });
+
+    this.formulario.valueChanges.subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.formulario) { return; }
+    const form = this.formulario;
+ 
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+ 
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+ 
+  formErrors = {
+    'nome': '',
+    'email': ''
+  };
+
+  validationMessages = {
+    'nome': {
+      'required':      'Name is required.',
+      'minlength':     'Name must be at least 4 characters long.',
+      'maxlength':     'Name cannot be more than 24 characters long.',
+      'forbiddenName': 'Someone named "Bob" cannot be a hero.'
+    },
+    'email': {
+      'required': 'Power is required.'
+    }
+  };
+
   onSubmit(){
+    this.submitted = true;
+    this.hero = this.formulario.value;
+    
      this.http.post('https://httpbin.org/post',JSON.stringify(this.formulario.value))
              .map(res => res)
              .subscribe(dados => console.log(dados));
